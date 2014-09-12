db7126b33626151d84e3bca8b9d1677d8b2f2362
Matt S <matt@theartofnerd.com>
2014-09-12 16:05:10 -0400

    Read in Lines
    Changed the way it handled files.
    Just drops lines directly into list (already stripped)
    Safer handling for OOB errors.

--- clExcuses2.py
+++ clExcuses2.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 from random import randint
 
-#Read in excuse library
-subject = [str(f) for f in file('subjects.lst').read().split("\n")]
-verb = [str(f) for f in file('excuses.lst').read().split("\n")]
-#excuses = [str(f) for f in file('excuseLibrary.lst').read().split("\n")]
+#Read in subject and excuses library and put stripped line into list
+subject = [line.strip() for line in open('subjects.lst')]
+verb = [line.strip() for line in open('excuses.lst')]
+
+#Seperate random Variables for Debugging
+randSubject = randint(0, len(subject) - 1)
+randExcuse = randint(0, len(verb) - 1) 
 
 #Draw a random excuse
-randomExcuse = subject[randint(0,len(subject) - 1)] + " "  + verb[randint(0,len(verb) - 1)]
+randomExcuse = subject[randSubject] + " "  + verb[randExcuse]
+
+#Debugging Outputs
+#print "Subject #: " + str(randSubject) +"; Excuse #: " + str(randExcuse) + "\n"
 
 #Print random excuse
 print randomExcuse
+
+#todo:
+#figure out why it's printing a blank subject
